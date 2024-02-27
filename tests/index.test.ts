d20e19b676d57457dd3cfe8ddc84763dfa98a0b1
Nimrod Astarhan <nimast@gmail.com>
2024-02-27 12:04:24 -0600

    add empty test to make jest happy

---
+++ tests/index.test.ts
@@ -0,0 +1,5 @@
+describe("currently doesn't test anything as this file is highly integrated into the gitbook runtime", () => {
+  it("life's beauty without logical conflict", () => {
+    expect(true).toBeTruthy();
+  });
+});
